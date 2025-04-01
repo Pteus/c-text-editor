f531fac2ee419685478210918fe2b633136053b0
Pedro Mateus <pedropmateus91@gmail.com>
2025-04-01 20:01:25 +0100

    Starting kilo text editor

---
+++ kilo.c
@@ -0,0 +1,85 @@
+/*** includes ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/errno.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
+struct termios orig_termios;
+
+/*** terminal ***/
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+void disableRawMode(void) {
+  // TCSAFLUSH - discards any unread input before applying the changes to the
+  // terminal.
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+void enableRawMode(void) {
+  // Terminal attributes can be read into a termios struct by tcgetattr()
+  // store original terminal settings
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    die("tcgetattr");
+
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  // ECHO is a bitflag, defined as 00000000000000000000000000001000 in binary.
+  // We use the bitwise-NOT operator (~) on this value to get
+  // 11111111111111111111111111110111. We then bitwise-AND this value with the
+  // flags field, which forces the fourth bit in the flags field to become 0,
+  // and causes every other bit to retain its current value. Flipping bits like
+  // this is common in C.
+
+  raw.c_cflag |= (CS8);
+  // turns off Ctrl-S and Ctrl-Q, carriage return and new line
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  // turns off ECHO, Canonical mode, CTRL C CTRL Z, CTRL v
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  // turns off "\n" to "\r\n"
+  raw.c_oflag &= ~(OPOST);
+
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  // apply the the changes
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    die("tcsetattr");
+}
+
+/*** init ***/
+
+int main(void) {
+  // terminal starts in canonical mode - the input is only sent to the program
+  // when enter is pressed this enales raw mode.
+  enableRawMode();
+
+  // reads 1 byte from the standard input into c, and keeps doing it until there
+  // are no more bytes to read
+  // press q to quit
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+      die("read");
+
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == CTRL_KEY('q')) {
+      break;
+    }
+  }
+  return 0;
+}
