f68c104c9fd73731ba6c5e92450db1e02e8a6917
Pedro Mateus <pedropmateus91@gmail.com>
2025-04-01 22:38:08 +0100

    Window size, the hard way
    ioctl() isn’t guaranteed to be able to request the window size on all
    systems, so we are going to provide a fallback method of getting the
    window size.

--- kilo.c
+++ kilo.c
@@ -14,8 +14,8 @@
 
 /*** data ***/
 struct editorConfig {
-  int screenRows;
-  int screenCols;
+  int screenrows;
+  int screencols;
   struct termios orig_termios;
 };
 
@@ -81,11 +81,32 @@ char editorReadKey(void) {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    return -1;
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+      break;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+  if (buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+    return -1;
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
-
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+      return -1;
+    return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
@@ -96,8 +117,11 @@ int getWindowSize(int *rows, int *cols) {
 /*** output ***/
 void editorDrawRows(void) {
   int y;
-  for (y = 0; y < E.screenRows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+  for (y = 0; y < E.screenrows; y++) {
+    write(STDOUT_FILENO, "~", 1);
+    if (y < E.screenrows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
@@ -137,7 +161,7 @@ void editorProcessKeypress(void) {
 /*** init ***/
 
 void initEditor(void) {
-  if (getWindowSize(&E.screenRows, &E.screenCols) == -1)
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
 
