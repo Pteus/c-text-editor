74086eee5b3e7cb0a4f87e0d58d9af72ff24d912
Pedro Mateus <pedropmateus91@gmail.com>
2025-04-01 23:18:56 +0100

    Hide the cursor when repainting

--- kilo.c
+++ kilo.c
@@ -1,5 +1,4 @@
 /*** includes ***/
-#include <_stdio.h>
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -154,12 +153,18 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 // clear the screen
-void editorRefreshScreen() {
+void editorRefreshScreen(void) {
   struct abuf ab = ABUF_INIT;
+
+  abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
+
   editorDrawRows(&ab);
+
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
+
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
 }
